78d82669b8f0438c3aa0766cb08f2d382f1ce52c
liucan <553670590@qq.com>
2018-08-16 23:14:16 +0800

    增加redis-pub/sub学习

--- src/main/java/com/liucan/common/redis/RedisMessageListener.java
+++ src/main/java/com/liucan/common/redis/RedisMessageListener.java
@@ -11,11 +11,12 @@ import org.springframework.stereotype.Component;
  * @date 2018/8/16
  * @brief redis接收到pub消息监听器
  * 参考资料：https://www.cnblogs.com/qianlizeguo/articles/6856813.html
- * 1.Pub/Sub功能
- * 2.消息订阅者，即subscribe客户端，需要独占链接，即进行subscribe期间，redis-client无法穿插其他操作，
- * 此时client以阻塞的方式等待“publish端”的消息；甚至需要在额外的线程中使用。
- * 3.消息发布者，即publish客户端，无需独占链接
- * 4.spring封装的pub/sub里面消息订阅者其实就是在额外线程里面执行的，可打断点调试
+ *          1.Pub/Sub功能
+ *          2.消息订阅者，即subscribe客户端，需要独占链接，即进行subscribe期间，redis-client无法穿插其他操作，
+ *            此时client以阻塞的方式等待“publish端”的消息；甚至需要在额外的线程中使用。
+ *          3.消息发布者，即publish客户端，无需独占链接
+ *          4.spring封装的pub/sub里面消息订阅者其实就是在额外线程里面执行的，可打断点调试
+ *          5.缺点是消息不是持久化的，发送就没有了
  */
 @Slf4j
 @Component
