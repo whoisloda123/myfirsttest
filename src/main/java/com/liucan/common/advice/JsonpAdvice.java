f09596b0540aa1be6a6db8e90bfa431bf35c1104
liucan <553670590@qq.com>
2018-09-26 22:53:40 +0800

    增加jsonp支持

---
+++ src/main/java/com/liucan/common/advice/JsonpAdvice.java
@@ -0,0 +1,16 @@
+package com.liucan.common.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
+/**
+ * @author liucan
+ * @date 2018/9/26
+ * @brief jsonp支持
+ */
+@ControllerAdvice
+public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+    public JsonpAdvice() {
+        super("callback");
+    }
+}
