e8ca19dd16702561d62d174a7bc9af913b9d604e
553650590@qq.com <553650590@qq.com>
2020-04-25 22:30:32 +0800

    refactor(日志管理): 优化日志管理代码

--- src/main/java/com/liucan/boot/persist/mybatis/mapper/LoggingEventExceptionMapper.java
+++ src/main/java/com/liucan/boot/persist/mybatis/mapper/LoggingEventExceptionMapper.java
@@ -1,10 +1,11 @@
 package com.liucan.boot.persist.mybatis.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liucan.boot.persist.mybatis.mode.LoggingEventException;
 
 /**
  * @author liucan
  * @version 2020/4/25
  */
-public interface LoggingEventExceptionMapper extends BaseMapper<LoggingEventExceptionMapper> {
+public interface LoggingEventExceptionMapper extends BaseMapper<LoggingEventException> {
 }
--- src/main/java/com/liucan/boot/service/db/LoggingService.java
+++ src/main/java/com/liucan/boot/service/db/LoggingService.java
@@ -6,7 +6,10 @@ import com.liucan.boot.persist.mybatis.mapper.LogginEventProperyMapper;
 import com.liucan.boot.persist.mybatis.mapper.LoggingEventExceptionMapper;
 import com.liucan.boot.persist.mybatis.mapper.LoggingEventMapper;
 import com.liucan.boot.persist.mybatis.mode.LoggingEvent;
+import com.liucan.boot.persist.mybatis.mode.LoggingEventException;
+import com.liucan.boot.persist.mybatis.mode.LoggingEventProperty;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +23,7 @@ import java.util.stream.Collectors;
  * @author liucan
  * @version 2020/4/25
  */
+@Slf4j
 @Service
 @AllArgsConstructor
 public class LoggingService {
@@ -36,20 +40,20 @@ public class LoggingService {
         long endTime = localDateTime.toInstant(ZoneOffset.of("+8")).toEpochMilli();
         long startTime = localDateTime.minusDays(1).toInstant(ZoneOffset.of("+8")).toEpochMilli();
 
+        log.info("[日志清除]开始清除日志，startTime:{},endTime:{}", startTime, endTime);
         Page<LoggingEvent> page;
         do {
             page = (Page<LoggingEvent>) loggingEventMapper.selectPage(new Page<>(1, 500),
                     Wrappers.<LoggingEvent>lambdaQuery().between(LoggingEvent::getTimestmp, startTime, endTime));
+
             List<LoggingEvent> records = page.getRecords();
             if (CollectionUtils.isNotEmpty(records)) {
                 List<Long> eventIds = records.stream().map(LoggingEvent::getEventId).collect(Collectors.toList());
                 loggingEventMapper.deleteBatchIds(eventIds);
-
-                loggingEventExceptionMapper.delete()
+                loggingEventExceptionMapper.delete(Wrappers.<LoggingEventException>lambdaUpdate().in(LoggingEventException::getEventId, eventIds));
+                logginEventProperyMapper.delete(Wrappers.<LoggingEventProperty>lambdaUpdate().in(LoggingEventProperty::getEventId, eventIds));
             }
         } while (page.hasNext());
-
-        loggingEventMapper.delete(Wrappers.<LoggingEvent>lambdaUpdate().between(LoggingEvent::getTimestmp, startTime, endTime)
-        )
+        log.info("[日志清除]结束清除日志，startTime:{},endTime:{}", startTime, endTime);
     }
 }
